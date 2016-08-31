fcab224cca07b3ecca30073193b953e8cee61e3f
Ankur Loriya <ankur.loriya@einfochips.com>
2016-08-31 19:48:31 +0530

    load image in DIV background implementation

--- js/css-sprite.js
+++ js/css-sprite.js
@@ -1,2 +1,41 @@
-js
+// Author : Ankur Loriya <ankur.loriya@gmail.com><about.me/ankurloriya>
 
+var constants = {
+    id: {
+        stageImage: "stage-img"
+    }
+}
+
+$(document).ready(function () {
+
+});
+
+function loadImage(e) {
+    debugger;
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+        var f = e.target.files[0];
+
+        if (f) {
+            var r = new FileReader();
+            r.onload = function (e1) {
+                var contents = e1.target.result;
+                //alert("Got the file.n" + "name: " + f.name + "n" + "type: " + f.type + "n" + "size: " + f.size + " bytesn" + "starts with: " + contents.substr(1, contents.indexOf("n")));
+                $("#" + constants.id.stageImage).css({
+                    "background-image": "url(" + contents + ")"
+                });
+            }
+            r.readAsDataURL(f);
+        } else {
+            alert('The File APIs are not fully supported by your browser.');
+        }
+    }
+}
+
+function loadImageInDom(imgPath, domElement) {
+
+
+}
+
+function validateImage() {
+
+}
